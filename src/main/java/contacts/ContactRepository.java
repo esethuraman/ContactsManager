e551392c375e5275b91fc5fea9630d1cc77daf0e
Sethuraman <esethuraman@paypal.com>
2018-09-21 12:14:36 -0400

    added post form. persistence layer yet to be added

--- src/main/java/contacts/ContactRepository.java
+++ src/main/java/contacts/ContactRepository.java
@@ -3,22 +3,31 @@ package contacts;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 @Component
 public class ContactRepository{
 
-    public void save(Contact contact) {
-    }
+    List<Contact> contacts;
 
-    public List<Contact> getAllContacts() {
+    public ContactRepository(){
+        contacts = new LinkedList<>();
         Contact contact = new Contact();
         contact.setId((long) 124235125);
         contact.setFirstName("Elavazhagan");
         contact.setLastName("Sethuraman");
         contact.setPhoneNumber("8978808128");
         contact.setEmailAddress("esethuraman@paypal.com");
+        contacts.add(contact);
+    }
 
-        return Arrays.asList(contact);
+    public void save(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public List<Contact> getAllContacts() {
+
+        return contacts;
     }
 }
