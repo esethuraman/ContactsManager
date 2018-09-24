dc0fdeeaeebf53777c0d5ead57d538da0d9387d6
Sethuraman <esethuraman@paypal.com>
2018-09-24 12:49:24 -0400

    security layer packages and a prototype added

--- src/main/java/contacts/WebSecurityConfiguration.java
+++ src/main/java/contacts/WebSecurityConfiguration.java
@@ -1,29 +1,41 @@
 package contacts;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-////
-//@Configuration
-//@EnableWebSecurity
-//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 //
-//    protected void configure(HttpSecurity http) throws Exception{
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/contacts").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+
 //        http
 //                .authorizeRequests()
 //                .antMatchers("/xyz").authenticated()
 //                .and()
 //                .formLogin().and()
 //                .httpBasic();
-//    }
-//
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("admin").password("password").roles("USER");
-//    }
-//
-//
-//}
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // {noop} prefix directs DelegatingPasswordEncoder to use NoOpPasswordEncoder
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("{noop}password").roles("USER");
+    }
+
+}
